58b3e49a58a4698510846227c32c39ae8e15d785
zaynabali-12345 <zaynabali00786@gmail.com>
2025-05-31 20:22:42 +0530

    Save  it

---
+++ FibonacciSeries.java
@@ -0,0 +1,13 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 30;
+        int a = 0, b = 1, c;
+        System.out.print("Fibonacci Series: " + a + ", " + b);
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            System.out.print(", " + c);
+            a = b;
+            b = c;
+        }
+    }
+}
